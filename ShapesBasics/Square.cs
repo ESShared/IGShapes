d6ce34c5c14e74a83b90819a51ee4bc87f2b81c5
tfsbuild <tfsbuild@infragistics.com>
2019-02-06 20:58:03 +0200

    Extending the comment string

--- ShapesBasics/Square.cs
+++ ShapesBasics/Square.cs
@@ -12,7 +12,7 @@ namespace ShapesBasics
 
         public int CalculateArea()
         {
-			// Return the area calculated
+			// Return the area calculated for a square
             return Length * Length;
         }
     }
